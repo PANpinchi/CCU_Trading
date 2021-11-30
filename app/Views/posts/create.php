c644f8efc48e8c777287a2ceb676123a0e66cb74
anc891203 <anc891203@gmail.com>
2021-11-30 20:02:58 +0800

    1

--- app/Views/posts/create.php
+++ app/Views/posts/create.php
@@ -14,7 +14,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="/style/post.css">
 
-        <title>中正大學買賣交流</title>
+        <title>中正大學買賣交流1</title>
     </head>
     
     <body>
