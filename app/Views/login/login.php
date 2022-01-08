21d1d5050b052ca6b182e454187ce5a904435d27
anc891203 <anc891203@gmail.com>
2022-01-09 03:18:24 +0800

    phone

--- app/Views/login/login.php
+++ app/Views/login/login.php
@@ -3,7 +3,7 @@
     <head>
         <!-- Required meta tags -->
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
 
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -14,8 +14,8 @@
 
     <body>
         <div class="row" style="user-select: none;">
-            <div class="col-md-4"></div>
-            <div class="col-md-4" align="center">
+            <div class="col-md-4 col-xs-4 col-sm-4 col-lg-4"></div>
+            <div class="col-md-4 col-xs-4 col-sm-4 col-lg-4" align="center">
                 <br><br><br>
                 <p class="type0">中 正 大 學 買 賣 交 流</p>
                 <br><br>
@@ -30,7 +30,7 @@
                     <p class="type4">※ 請使用學校電子郵件登入</p>
                 </form>
             </div>
-            <div class="col-md-4" style="position: absolute; bottom: 10px; right: 0px; text-align: right;">
+            <div class="col-md-4 col-xs-4 col-sm-4 col-lg-4" style="position: absolute; bottom: 10px; right: 0px; text-align: right;">
                     <a href="/AdminController/admin_login" type="button" class="btn btn-secondary" style="font-weight: bold; opacity: .5;">管理員登入</a>
             </div>
         </div>
